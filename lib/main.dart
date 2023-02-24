6791fae3b7daa7b70223ad096d3ac60baf4e85a3
shreeecreation <shreeecreation720#gmail.com>
2023-02-24 13:07:08 +0545

    Feat: Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:kagajpatra/firebase_options.dart';
 import 'src/app/screens/homescreen/presentation/home_screen.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
